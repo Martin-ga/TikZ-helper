88c27c1863b7affece517ce3b598af4bef3348e7
Martin-ga <martingomezabejonmg@gmail.com>
2022-10-26 18:53:05 +0200

    Added automatic processing

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <filesystem>
 #include "iterativeCollage.h"
 #include "fileManagement.h"
 #include "globalConstants.h"
 #include "commandsManager.h"
 using namespace std;
 
-int main()
+int processCurrentRoute()
 {
-    cout << "Please, enter the route you would like to compile.\n";
-    getline(cin, ROUTE);
-    ROUTE += "/";
     vector<string> commands = getLinesOfFile(COMMANDS_FILE);
     map<string,BasicTikZpicture> picturesData;
     for (string s : commands)
@@ -21,5 +19,37 @@ int main()
         cout << "The command was successfully processed.\n";
     }
     cout << "Have a nice day.\n" << flush;
+    return 0;
+}
+
+int main()
+{
+    bool work = false;
+    string path = ".";
+    for (const auto& entry : filesystem::directory_iterator(path))
+    {
+        if (entry.path().string() == "./.tikzhelperWork")
+        {
+            work = true;
+        }
+    }
+    if (!work)
+    {
+        cout << "Work file not found, leaving now.\n";
+        cout << "If you want TikZhelper to work, add a file .tikzhelperWork and all directories will be processed.\n";
+        return 0;
+    }
+    cout << "Work file found.\n";
+    for (const auto& entry : filesystem::directory_iterator(path))
+    {
+        if (entry.is_directory())
+        {
+            ROUTE = entry.path().string();
+            ROUTE += "/";
+            cout << "Processing directory " << ROUTE << " ...\n";
+            processCurrentRoute();
+            cout << "Route processed successfully.\n";
+        }
+    }
     return 0;
 }
